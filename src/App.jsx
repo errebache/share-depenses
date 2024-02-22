c835e2da739c05992aee5036ebea7747323dc7df
errebache <mouhcine.errebache@gmail.com>
2024-02-23 00:42:41 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios';
-import './App.scss'
+import { Suspense, useEffect, useState } from "react";
+import axios from "axios";
+import { Outlet, useLocation } from "react-router-dom";
+import Homepage from "./pages/homepage/Homepege";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import AuthProvider from "./components/providers/AuthProvider";
+import { Provider } from "react-redux";
+import { store } from "./_store";
+import { Alert } from "./components/common/_components/Alert";
+import Loading from "./components/loaders/loading";
 
 function App() {
-
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    axios.get('/api/users')
-    .then(response => {
-      setData(response.data);
-    })
-    .catch(error => {
-      console.error("Erreur lors de la récupération des données", error);
-    });
-  }, []);
-
-  if (!data) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <div>
-        <h1>Données reçues du serveur</h1>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  )
+    <Provider store={store}>
+      <Alert />
+      <AuthProvider>
+        <Header />
+        <Suspense fallback={<Loading />}>
+          <Outlet />
+        </Suspense>
+        <Footer />
+      </AuthProvider>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import "./assets/sass/main.scss"
+import router from "./router"
+import { RouterProvider } from 'react-router-dom'
+import i18n from './_config/i18n/i18n'
+
+
+i18n.init();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} i18n={i18n}></RouterProvider>
   </React.StrictMode>,
 )
